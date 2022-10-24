e823f6fa524c8e24689458bdeb929d577e0c6ca5
Nikita Klimoyts <everygreen22@yandex.ru>
2022-10-24 17:24:58 +0300

    fixed comments

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,17 +1,24 @@
 from django.urls import include, path
+from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework.routers import DefaultRouter
 
 from .views import CommentViewSet, GroupViewSet, PostViewSet
 
 
+app_name = 'api'
+
 router = DefaultRouter()
 
 router.register('posts', PostViewSet, basename='posts')
-router.register('posts/(?P<posts_id>\\d+)/comments', CommentViewSet,
-                basename='comments')
+router.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comment'
+)
 router.register('groups', GroupViewSet, basename='groups')
 
 
 urlpatterns = [
     path('v1/', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token),
 ]
